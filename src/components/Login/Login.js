f53c950a7630778a9702b269e52e51e04d1e7663
Luis Eduardo Ramirez Torres <luis.torres@corebiz.ag>
2022-09-22 23:53:10 -0500

    avance de login y registro

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,36 +4,38 @@ import style from "./Login.module.scss"
 const Login = ()=>{
     return(
         <div>
-            <div class="card text-center">
+            <div className="card text-center">
                 {/* <div class="card-header">Featured</div> */}
 
-                <div class="card-body mt-5">
-                    <h5 class="card-title">Welcome to DEV Community </h5>
-                    <p class="card-text">DEV Community is a community of 900,533 amazing developers</p>
+                <div className="card-body mt-5">
+                    <h5 className="card-title">Welcome to DEV Community </h5>
+                    <p className="card-text">DEV Community is a community of 900,533 amazing developers</p>
                     
-                    <div class="d-grid gap-2 col-3 mx-auto">
-                        <button class="btn bg-dark btn-primary" type="button">Continue with Apple</button>
-                        <button class="btn bg-warning btn-primary" type="button">Continue with Google</button>
-                        <button class="btn bg-dark btn-primary" type="button">Continue with Github</button>
-                        <button class="btn bg-info btn-primary" type="button">Continue with Twitter</button>
+                    <div className="d-grid gap-2 col-lg-5 col-sm-5 mx-auto">
+                        <button className="btn bg-dark btn-primary" type="button">Continue with Apple</button>
+                        <button className="btn bg-warning btn-primary" type="button">Continue with Google</button>
+                        <button className="btn bg-dark btn-primary" type="button">Continue with Github</button>
+                        <button className="btn bg-info btn-primary" type="button">Continue with Twitter</button>
                     </div>
                     
                     <br/>
                     
-                    <h5 class="">Email</h5>
+                    <h5 className="">Email</h5>
                     
-                    <input class="col-3 emailInput" type="text"/>
+                    <input className="col-lg-5 col-sm-5 emailInput" type="text"/>
                     
                     <br/>
 
-                    <h5 class="mt-3">Password</h5>
+                    <h5 className="mt-3">Password</h5>
                     
-                    <input class="col-3 passwordInput" type="password"/>
+                    <input className="col-lg-5 col-sm-5 passwordInput" type="password"/>
           
                     <br/>
-
-                    <a href="#" class="loginBtn btn btn-primary mt-3">Continue</a>
-                    <a href="#" class="registerBtn btn btn-primary ms-4 mt-3">Create account</a>
+                    
+                        <a href="#" className="loginBtn col-lg-5 col-sm-5 btn btn-primary mt-3 mb-3">Continue</a>
+                        <br/>
+                        <a href="#" className="registerBtn  ms-4 mt-3 pt-3">I forgot my password</a>
+                    
                 </div>
 
                 {/* <div class="card-footer text-muted">2 days ago</div> */}
